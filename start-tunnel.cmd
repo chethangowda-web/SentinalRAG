49663fff3270363365f442db63eca2779a8fecd2
chethangowda-web <iamchethan2813@gmail.com>
2026-07-21 23:42:51 +0530

    Add .exe/.cmd to .gitignore, remove tracked binaries

--- start-tunnel.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-set LOGFILE=%USERPROFILE%\Desktop\sentinelrag-url.txt
-set TUNNELLOG=%TEMP%\cloudflared.log
-
-echo Starting SentinelRAG Tunnel... > "%LOGFILE%"
-echo %date% %time% >> "%LOGFILE%"
-
-"C:\Program Files (x86)\cloudflared\cloudflared.exe" tunnel --url http://localhost:80 > "%TUNNELLOG%" 2>&1
